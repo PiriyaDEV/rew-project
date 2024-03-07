7a518c2760f66fc95f052f8e859699ab5075197c
Piriya <pryeditor@gmail.com>
2024-03-07 10:30:47 +0700

    add form and extension excel

--- src/pages/Mainpage.js
+++ src/pages/Mainpage.js
@@ -1,33 +1,105 @@
-import React from "react";
+import React, { useState } from "react";
 import Upload from "../components/Upload";
-import '../assets/css/mainpage.css'
+import { performanceIndicatorOptions, studentOutcomeOptions } from "../utils";
+import "../assets/css/mainpage.css";
 
 export default function Mainpage() {
+  const [formData, setFormData] = useState({
+    course: "",
+    studentOutcome: "",
+    performanceIndicator: "",
+    direct: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
   return (
     <div className="App">
       <div className="container">
         <div className="upload-section">
-          <Upload/>
-        </div>  
+          <Upload />
+        </div>
         <div className="form-section">
           <form>
             <label htmlFor="course">Course *</label>
-            <input type="text" id="course" name="course" required />
-            
+            <input
+              type="text"
+              id="course"
+              name="course"
+              required
+              value={formData.course}
+              onChange={handleChange}
+            />
+
             <label htmlFor="outcome">Student Outcome</label>
-            <input type="text" id="outcome" name="outcome" />
+            <select
+              id="outcome"
+              name="studentOutcome"
+              value={formData.studentOutcome}
+              onChange={handleChange}
+            >
+              <option value="">Select</option>
+              {studentOutcomeOptions.map((option) => (
+                <option key={option.value} value={option.value}>
+                  {option.label}
+                </option>
+              ))}
+            </select>
 
             <label htmlFor="direct">Direct</label>
-            <input type="text" id="direct" name="direct" />
+            <div className="flex items-center gap-[10px]">
+              <label htmlFor="directYes">Yes</label>
+              <input
+                type="radio"
+                id="directYes"
+                name="direct"
+                value="Yes"
+                onChange={handleChange}
+              />
+              <label htmlFor="directNo">No</label>
+              <input
+                type="radio"
+                id="directNo"
+                name="direct"
+                value="No"
+                onChange={handleChange}
+              />
+            </div>
 
             <label htmlFor="indicator">Performance Indicator</label>
-            <input type="text" id="indicator" name="indicator" />
+            <select
+              id="indicator"
+              name="performanceIndicator"
+              value={formData.performanceIndicator}
+              onChange={handleChange}
+            >
+              <option value="">Select</option>
+              {performanceIndicatorOptions.map((option) => (
+                <option key={option.value} value={option.value}>
+                  {option.label}
+                </option>
+              ))}
+            </select>
           </form>
         </div>
         <div className="outcome-section">
           <div className="example-outcome">Example Outcome</div>
           <button className="download-button">Download (Excel)</button>
         </div>
+      </div>
+
+      {/* Remove it later */}
+      <div className="flex justify-center items-center gap-[20px]">
+        <span>course : {formData.course}</span>
+        <span>outcome : {formData.studentOutcome}</span>
+        <span>direct : {formData.direct}</span>
+        <span>performance indicator : {formData.performanceIndicator}</span>
       </div>
     </div>
   );
---
+++ src/utils.js
@@ -0,0 +1,9 @@
+export const performanceIndicatorOptions = [
+  { value: "PI1", label: "PI1" },
+  { value: "PI2", label: "PI2" },
+];
+
+export const studentOutcomeOptions = [
+  { value: "SO1", label: "SO1" },
+  { value: "SO2", label: "SO2" },
+];

b803b0ef73ab8e64451ab7eeebdce20ef6a107d4
Piriya <pryeditor@gmail.com>
2024-03-07 09:41:16 +0700

    initial project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import "./assets/css/page.css";
+
+import Mainpage from "./pages/Mainpage.js";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/">
+          <div className="App">
+            <Navbar />
+            <Mainpage />
+          </div>
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Mainpage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Mainpage() {
+  return (
+    <div id="mainpage" className="section">
+      <div className="page-container">
+        <h1>This is Mainpage (Already install Tailwind)</h1>
+      </div>
+    </div>
+  );
+}
